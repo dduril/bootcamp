6f7b29a8d2c973ae36abcf8c209135a36c64774b
Doug Duril <doug.duril@gmail.com>
2016-07-28 23:24:49 -0700

    Adding Python string examples

---
+++ python/13-strings/strings.py
@@ -0,0 +1,17 @@
+# -----------------------------------------------
+# Python Examples
+#
+# Strings
+# -----------------------------------------------
+
+def main():
+    str = "Hello World!"
+    print(type(str))
+    print("str:\t\t\t{}".format(str))
+    print("lower():\t\t{}".format(str.lower()))
+    print("upper():\t\t{}".format(str.upper()))
+    print("capitalize():\t{}".format(str.capitalize()))
+    print("title():\t\t{}".format(str.title()))
+    print("len(str):\t\t{}".format(len(str)))
+
+if __name__ == "__main__": main()
