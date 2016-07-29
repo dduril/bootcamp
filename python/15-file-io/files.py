4a9839e6107217d665bddd4a4a60f3a5f28bff72
Doug Duril <doug.duril@gmail.com>
2016-07-29 09:13:23 -0700

    Modification of Python file example

--- python/15-file-io/files.py
+++ python/15-file-io/files.py
@@ -7,9 +7,11 @@
 def main():
     try:
         fh = open('../raven.txt', 'r')
+        out_file = open('../new.txt', 'w')
         for line in fh.readlines():
-            print(line, end='')
+            print(line, file = out_file, end='')
     except IOError as e:
         print("Exception ({})".format(e))
-
+    print("Done")
+    
 if __name__ == "__main__": main()
