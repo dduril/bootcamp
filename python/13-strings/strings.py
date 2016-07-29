fbfcb55a655f82496bfcc337b9cdc0c01014a9d3
Doug Duril <doug.duril@gmail.com>
2016-07-28 23:35:38 -0700

    Working on strings.py

--- python/13-strings/strings.py
+++ python/13-strings/strings.py
@@ -7,11 +7,13 @@
 def main():
     str = "Hello World!"
     print(type(str))
+    print("id():\t\t\t{}".format(id(str)))
     print("str:\t\t\t{}".format(str))
     print("lower():\t\t{}".format(str.lower()))
     print("upper():\t\t{}".format(str.upper()))
     print("capitalize():\t{}".format(str.capitalize()))
     print("title():\t\t{}".format(str.title()))
+    print("swapcase():\t\t{}".format(str.swapcase()))
     print("len(str):\t\t{}".format(len(str)))
 
 if __name__ == "__main__": main()
