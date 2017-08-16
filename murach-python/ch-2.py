354a11dd272cf5232e411cf9a8bbf60770fba714
Doug Duril <doug.duril@gmail.com>
2017-08-16 08:20:43 -0700

    Add python chapter 2 examples

--- murach-python/ch-2.py
+++ murach-python/ch-2.py
@@ -3,3 +3,104 @@ print("Hello World!")
 print()
 print("Bye!")
 
+# initialize variables and assign values
+first_name = "John"
+quantity1 = 25
+quantity2 = 42
+list_price = 10.99
+
+# naming conventions for variables
+variable_name = 1       # underscore notation
+variableName = 2        # camel case
+
+# code that calculates sales tax
+subtotal = 100.00
+tax_percent = .08
+tax_amount = subtotal * tax_percent
+grand_total = subtotal + tax_amount
+
+# code that calculates the perimeter of a rectangle
+width = 5.25
+length = 10.75
+perimeter = (2 * width) + (2 * length)
+
+# ways to increment a variable
+counter = 0
+counter = counter + 1
+counter += 1
+
+# assigning strings to variables
+first_name = "John"
+last_name = "Smith"
+full_name = "Mr. Anderson"
+
+# concatenating strings together
+name = first_name + " " + last_name
+
+# joining a string with a number
+age = 42
+message = name + " is " + str(age) + " years old."
+
+# implicit continuation of a string over several lines
+score_total = 370
+average_score = 92.5
+print("Total Score: "
+      + str(score_total)
+      + "\nAverage Score: "
+      + str(average_score))
+
+# new line character
+print("Title: Python Programming\nQuantity: 10")
+
+# tab and new line characters
+print("Title:\t\tPython Programming\nQuantity:\t10")
+
+# backslash in a Windows path
+print("C:\\development\\python\\examples")
+
+# print() function examples
+print(19.99)
+print("Price", 19.99)
+print(1, 2, 3, 4, 5)
+print("Total Score:", score_total, "\nAverage Score:", average_score)
+print("Total Score: " + str(score_total) + "\nAverage Score: " + str(average_score))
+print(1, 2, 3, 4, 5, sep=' | ')
+print(1, 2, 3, 4, 5, end='Done')
+
+# getting string input from a user
+first_name = input("Enter your first_name: ")
+print("Hello, " + first_name + ".")
+
+# getting int or float input from a user
+quantity = int(input("Enter quantity: "))
+price = float(input("Enter price: "))
+
+# using the round() function
+miles_driven = 150
+gallons_used = 5.875
+mpg = round(miles_driven / gallons_used, 2)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
