e92053994bef884a20c98779c6889510f848774b
Doug Duril <doug.duril@gmail.com>
2017-08-16 09:05:51 -0700

    Add Java chapter 1 example

---
+++ murach-java/ch-1.java
@@ -0,0 +1,17 @@
+package ch01_test;
+
+/**
+ *
+ * @author Doug
+ */
+public class TestApp {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hello World!");
+    }
+    
+}
