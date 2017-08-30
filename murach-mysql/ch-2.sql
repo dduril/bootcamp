5ffc1f5a42f8f2793ef72c591b875951752591ae
Doug Duril <doug.duril@gmail.com>
2017-08-30 16:07:44 -0700

    Add MySQL ch-2 examples

--- murach-mysql/ch-2.sql
+++ murach-mysql/ch-2.sql
@@ -3,3 +3,39 @@ MySQL Examples - MySQL Workbench and MySQL Command Line Client
 Chapter 2
 */
 
+SELECT vendor_name 
+FROM vendors;
+
+SELECT COUNT(*) AS number_of_invoices,
+    SUM(invoice_total) AS grand_invoice_total
+FROM invoices;
+
+SELECT vendor_name, vendor_city, vendor_state
+FROM vendors
+ORDER BY vendor_name;
+
+SELECT COUNT(*) AS number_of_invoices,
+    SUM(invoice_total - payment_total - credit_total) AS total_due
+FROM invoices
+WHERE invoice_total - payment_total - credit_total > 0;
+
+SELECT vendor_name, vendor_city
+FROM vendors
+WHERE vendor_id = 34;
+
+SELECT COUNT(*) AS number_of_invoices,
+    SUM(invoice_total - payment_total - credit_total) AS total_due
+FROM invoices
+WHERE vendor_id = 34;
+
+/*
+-- Using the MySQL Command Line Client
+
+mysql> show databases;
+
+mysql> use ap;
+
+mysql> select invoice_number from invoices limit 10;
+
+mysql> describe ap;
+*/
