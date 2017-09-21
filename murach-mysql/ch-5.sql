aa90ef993c90fe2e172106fb0482539623aaee46
Doug Duril <doug.duril@gmail.com>
2017-09-21 09:27:07 -0700

    Minor edit to MySQL ch-5.sql

--- murach-mysql/ch-5.sql
+++ murach-mysql/ch-5.sql
@@ -14,9 +14,11 @@ SELECT * FROM invoices
 WHERE invoice_total - payment_total - credit_total = 0;
 
 CREATE TABLE vendor_balances AS
-SELECT vendor_id, SUM(invoice_total) AS sum_of_invoices
+SELECT 
+    vendor_id, 
+    SUM(invoice_total) AS sum_of_invoices
 FROM invoices
-WHERE (invoice_total - payment_total - credit_total) <> 0
+    WHERE (invoice_total - payment_total - credit_total) <> 0
 GROUP BY vendor_id;
 
 DROP TABLE old_invoices;
