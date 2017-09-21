9d1f45783d3da495d0d6dd477bce4ac12b2ee4bf
Doug Duril <doug.duril@gmail.com>
2017-09-21 14:08:01 -0700

    Add MySQL ch-8 examples

--- murach-mysql/ch-8.sql
+++ murach-mysql/ch-8.sql
@@ -3,3 +3,19 @@
  * MySQL Examples - Data Types
  * ----------------------------------------------
  */
+
+ /* CONCAT $ to invoice_total */
+SELECT 
+    invoice_date,
+    invoice_number,
+    CONCAT('$', invoice_total) as invoice_total
+FROM invoices
+ORDER BY invoice_date DESC;
+
+/* CONCAT full vendor name and address */
+SELECT CONCAT(vendor_name, ', ', vendor_address1, ', ',
+       vendor_city, ', ', vendor_state, ' ', vendor_zip_code)
+       AS vendor_full_address
+FROM vendors
+WHERE vendor_name LIKE 'A%'
+ORDER BY vendor_name;
