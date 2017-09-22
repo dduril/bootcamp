cefdd99be7546418fa069f6a0d045b07ac9f64bd
Doug Duril <doug.duril@gmail.com>
2017-09-22 10:24:21 -0700

    Add MySQL ch-10 example

--- murach-mysql/ch-10.sql
+++ murach-mysql/ch-10.sql
@@ -4,3 +4,13 @@
  * ----------------------------------------------
  */
 
+/*
+Designing Data Structures
+  
+1. Identify data elements 
+2. Break down elements into smallest useable sub-elements
+3. Determine tables and columns
+4. Determine primary and foreign keys
+5. Normalize data structure
+6. Add indexes 
+*/
