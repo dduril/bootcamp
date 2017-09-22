1ac0b900d7d666258b814eb1619cd89e2ef92495
Doug Duril <doug.duril@gmail.com>
2017-09-21 18:02:18 -0700

    Add MySQL ch-9 examples

--- murach-mysql/ch-9.sql
+++ murach-mysql/ch-9.sql
@@ -37,3 +37,35 @@ SELECT
     MONTHNAME(invoice_date) AS month_name
 FROM invoices
 WHERE invoice_date = '2014-08-02';
+
+/* date formatting examples */
+SELECT 
+    invoice_date,
+    DATE_FORMAT(invoice_date, '%m/%d/%y') AS 'm/d/y',
+    DATE_FORMAT(invoice_date, '%W, %M %D, %Y') AS 'W, M D Y',
+    DATE_FORMAT(invoice_date, '%e-%b-%y') AS 'e-b-y'
+FROM invoices
+WHERE invoice_date = '2014-08-02';
+
+/* CASE examples */
+SELECT invoice_number, terms_id,
+    CASE terms_id
+        WHEN 1 THEN 'Net due 10 days'
+        WHEN 2 THEN 'Net due 20 days'
+        WHEN 3 THEN 'Net due 30 days'
+        WHEN 4 THEN 'Net due 60 days'
+        WHEN 5 THEN 'Net due 90 days'
+    END AS terms
+FROM invoices
+ORDER BY terms DESC;
+
+SELECT invoice_number, invoice_total, invoice_date, invoice_due_date,
+    CASE 
+        WHEN DATEDIFF(DATE('2014-08-15'), invoice_due_date) > 30
+            THEN 'Over 30 days past due'
+        WHEN DATEDIFF(DATE('2014-08-15'), invoice_due_date) > 0
+            THEN '1 to 30 days past due'
+        ELSE 'Current'
+    END AS invoice_status
+FROM invoices
+WHERE invoice_total - payment_total - credit_total > 0;
