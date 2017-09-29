7d0138c28801957126f4d89b172fbd28605f0aa8
Doug Duril <doug.duril@gmail.com>
2017-09-29 11:51:02 -0700

    Add MySQL ch-15 and ch-16 examples

--- murach-mysql/ch-15.sql
+++ murach-mysql/ch-15.sql
@@ -4,3 +4,59 @@
  * ------------------------------------------------
  */
  
+USE ap;
+
+-- test insert_invoice
+-- ..............................................................
+CALL insert_invoice(34, 'ZXA-080', '2015-01-18', 14092.59, 
+                    3, '2015-03-18');
+CALL insert_invoice(34, 'ZXA-082', '2015-01-18', 14092.59,
+                    NULL, NULL);
+
+-- this statement raises an error
+CALL insert_invoice(34, 'ZXA-083', '2015-01-18', -14092.59,
+                    NULL, NULL);
+
+-- clean up
+SELECT * FROM invoices WHERE invoice_id >= 115;
+
+-- DELETE FROM invoices WHERE invoice_id >= 115;
+
+
+-- test select_invoices
+-- ..............................................................
+CALL select_invoices('2014-07-25', 100);
+
+CALL select_invoices('2014-07-25', NULL);
+
+CALL select_invoices(NULL, 1000);
+
+CALL select_invoices(NULL, NULL);
+
+
+-- test get_vendor_id
+-- ..............................................................
+SELECT invoice_number, invoice_total
+FROM invoices
+WHERE vendor_id = get_vendor_id('IBM');
+
+
+-- test get_balance_due
+-- ..............................................................
+SELECT vendor_id, invoice_number, 
+       get_balance_due(invoice_id) AS balance_due 
+FROM invoices
+WHERE vendor_id = 37;
+
+
+-- test get_balance_due, get_sum_balance_due
+-- ..............................................................
+SELECT vendor_id, invoice_number, 
+       get_balance_due(invoice_id) AS balance_due, 
+       get_sum_balance_due(vendor_id) AS sum_balance_due
+FROM invoices
+WHERE vendor_id = 37;
+
+-- DROP FUNCTION get_sum_balance_due;
+
+
--- murach-mysql/ch-16.sql
+++ murach-mysql/ch-16.sql
@@ -4,3 +4,55 @@
  * ----------------------------------------------
  */
  
+USE ap;
+
+-- test vendors_before_update
+-- ..............................................................
+UPDATE vendors
+SET vendor_state = 'wi'
+WHERE vendor_id = 1;
+
+SELECT vendor_name, vendor_state
+FROM vendors
+WHERE vendor_id = 1;
+
+
+-- test invoices_before_update
+-- ..............................................................
+UPDATE invoices
+SET invoice_total = 600
+WHERE invoice_id = 100;
+
+SELECT invoice_id, invoice_total, credit_total, payment_total 
+FROM invoices WHERE invoice_id = 100;
+
+
+-- test invoices_audit, invoices_after_insert, invoices_after_delete
+-- ..............................................................
+-- make sure that there is at least one record to delete
+INSERT INTO invoices VALUES 
+(115, 34, 'ZXA-080', '2014-08-30', 14092.59, 0, 0, 3, '2014-09-30', NULL);
+
+DELETE FROM invoices WHERE invoice_id = 115;
+
+SELECT * FROM invoices_audit;
+
+-- clean up
+-- DELETE FROM invoices_audit;
+
+
+-- test TRIGGERS
+-- ..............................................................
+SHOW TRIGGERS;
+SHOW TRIGGERS IN ex;
+SHOW TRIGGERS IN ap LIKE 'ven%';
+
+-- DROP TRIGGER vendors_before_update;
+-- DROP TRIGGER IF EXISTS vendors_before_update;
+
+
+-- test EVENTS
+-- ..............................................................
+SHOW VARIABLES LIKE 'event_scheduler';
+SHOW EVENTS;
+SHOW EVENTS IN ap;
