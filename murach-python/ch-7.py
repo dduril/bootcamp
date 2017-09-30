7296f0ceb22604dd84d17f213542d346974a49cb
Doug Duril <doug.duril@gmail.com>
2017-09-30 07:12:21 -0700

    Update Python ch-7 examples

--- murach-python/ch-7.py
+++ murach-python/ch-7.py
@@ -3,6 +3,9 @@
 #   File I/O
 # -------------------------------------
 
+# Working with text files
+# -------------------------------------
+
 # open a text file in write mode and manually close file
 outFile = open("test.txt", "w")
 outFile.write("Test")
@@ -32,14 +35,16 @@ with open("authors.txt") as file:
 
 # write and read a list of strings
 # write the items in a list to a file
+FILENAME = "movies.txt"
+
 movies = ["Casablanca", "It's a Wonderful Life", "Rear Window"]
-with open("movies.txt", "w") as file:
+with open(FILENAME, "w") as file:
     for m in movies:
         file.write(m + "\n")
 
 # read the lines in a file into a list
 movies = []
-with open("movies.txt") as file:
+with open(FILENAME) as file:
     for line in file:
         line = line.replace("\n", "")
         movies.append(line)
@@ -47,7 +52,41 @@ print(movies)
 
 # write and read a list of numbers
 # write the items in a list to a file
+FILENAME = "arthur-c-clarke.txt"
+
 years = [2001, 2010, 2061, 3001]
-#with open("SpaceOdyssey.txt", "w") as file
+with open(FILENAME, "w") as file:
+    for year in years:
+        file.write(str(year) + "\n")
 
 # read the items in a list from a file
+years = []
+with open(FILENAME, "w") as file:
+    for line in file:
+        line = line.replace("\n", "")
+        years.append(int(line))
+print(years)
+
+# Working with CSV files
+# -------------------------------------
+
+movies = [["Star Wars", 1977],
+         ["The Empire Strikes Back", 1980],
+         ["Return of the Jedi", 1983]]
+
+# import the csv module
+import csv
+
+# write to the csv file
+with open("star-wars-movies.txt", "w", newline="") as file:
+    writer = csv.writer(file)
+    writer.writerows(movies)
+
+# read from the csv file
+with open("star-wars-movies.txt", newline="") as file:
+    reader = csv.reader(file)
+    for row in reader:
+        print(row[0] + " (" + str(row[1]) + ")")
+
+# Working with binary files
+# -------------------------------------
