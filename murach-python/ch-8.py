6f03940b709c1d2eee1e302fd96f400de84f8dd1
Doug Duril <doug.duril@gmail.com>
2017-09-30 20:17:52 -0700

    Start Python ch-8 examples

--- murach-python/ch-8.py
+++ murach-python/ch-8.py
@@ -2,3 +2,39 @@
 # Chapter 8 Code Examples
 #   Exception Handling
 # -------------------------------------
+
+# handle ValueError exception
+try:
+    number = int(input("Enter an integer: "))
+    print("You entered a valid integer: " + str(number) + ".")
+except ValueError:
+    print("You entered an invalid integer. Please try again.")
+
+# handle all exceptions
+try:
+    number = int(input("Enter an integer: "))
+    print("You entered a valid integer: " + str(number) + ".")
+except:
+    print("You entered an invalid integer. Please try again.")
+
+# handling multiple exceptions
+filename = input("Enter filename: ")
+movies = []
+try:
+    with open(filename) as file:
+        for line in file:
+            line = line.replace("\n", "")
+            movies.append(line)
+except FileNotFoundError:
+    print("File not found: " + filename)
+except OSError:
+    print("File found. Error reading file.")
+except Exception:
+    print("An unexpected error has occurred.")
+
+# handling multiple exceptions using type() and exit()
+
+# import the sys module
+import sys
+
+
