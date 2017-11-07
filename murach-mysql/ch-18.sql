9a66e5f2462d1b65acdf8d97c1670fbd4ece2905
Doug Duril <doug.duril@gmail.com>
2017-11-07 12:34:00 -0800

    Finish MySQL examples for ch-18

--- murach-mysql/ch-18.sql
+++ murach-mysql/ch-18.sql
@@ -123,3 +123,50 @@ FROM ap_admin, john@localhost;
 
 REVOKE UPDATE, DELETE
 ON ap.invoices FROM john@localhost
+
+/* Changing Passwords Examples */
+SET PASSWORD FOR john = PASSWORD('pa55word');
+
+SET PASSWORD = PASSWORD('secret');
+
+GRANT USAGE ON *.* TO john IDENTIFIED BY 'pa55word';
+
+SELECT Host, User
+FROM mysql.user
+WHERE Password = '';
+
+/* Full Script for Creating Users */
+-- drop the users (causes an error if they don't exist yet)
+DROP USER john;
+DROP USER jane;
+DROP USER jim;
+DROP USER john@localhost;
+
+-- create the users
+CREATE USER john IDENTIFIED BY 'sesame';
+CREATE USER jane IDENTIFIED BY 'sesame';
+CREATE USER jim IDENTIFIED BY 'sesame';
+CREATE USER john@localhost IDENTIFIED BY 'sesame';
+
+-- grant privileges to the ap_developer (john)
+GRANT ALL ON *.* TO john@localhost WITH GRANT OPTION;
+
+-- grant privileges to the ap manager (jim)
+GRANT SELECT, INSERT, UPDATE, DELETE ON ap.* TO jim;
+GRANT USAGE ON ap.* TO jim WITH GRANT OPTION;
+
+-- grant privileges to ap users (john, jane)
+GRANT SELECT, INSERT, UPDATE, DELETE ON ap.vendors TO john, jane;
+GRANT SELECT, INSERT, UPDATE, DELETE ON ap.invoices TO john, jane;
+GRANT SELECT, INSERT, UPDATE, DELETE ON ap.invoice_line_items TO john, jane;
+GRANT SELECT ON ap.general_ledger_accounts TO john, jane;
+GRANT SELECT ON ap.terms TO john, jane;
+
+-- view user account data
+SELECT User, Host, Password FROM mysql.user;
+
+-- view the privileges for each user
+SHOW GRANTS FOR john;
+SHOW GRANTS FOR jane;
+SHOW GRANTS FOR jim;
+SHOW GRANTS FOR john@localhost;
