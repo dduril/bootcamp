13008c88adcb29a9c87ec6d7b140dd64ed12b91b
Doug Duril <doug.duril@gmail.com>
2017-11-07 13:00:17 -0800

    Add MySQL ch-19 examples

--- murach-mysql/ch-19.sql
+++ murach-mysql/ch-19.sql
@@ -5,3 +5,24 @@
  * ----------------------------------------------
  */
  
+/* 
+   -- MySQL bin directory for Windows
+   
+   cd /program files/mysql/mysql server 5.6/bin
+
+   -- MySQL bin directory for Mac OS X and Unix/Linux
+
+   cd /usr/local/mysql/bin
+*/
+
+/* Running the mysqldump program */
+/*
+-- for a single database
+mysqldump movies > /development/mysql/movies-backup-11-07-2017.sql -u root -p
+
+-- for specified databases
+mysqldump --databases books movies sales mysql > /development/mysql/multiple-db-backup-11-07-2017.sql -u root -p
+
+-- for all databases
+mysqldump --all-databases > /development/mysql/all-db-backup-11-07-2017.sql -u root -p
+*/
