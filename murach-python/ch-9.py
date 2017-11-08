c9ca4cde15f449a6d9a748f819c337f97d7a7aa2
Doug Duril <doug.duril@gmail.com>
2017-11-07 21:06:02 -0800

    Add Python ch-9 examples

--- murach-python/ch-9.py
+++ murach-python/ch-9.py
@@ -3,7 +3,8 @@
 #   Working with Numbers
 # -------------------------------------
 
-# import the math module
+# import the locale and math modules
+import locale as lc
 import math as m
 
 # example with floating-point error
@@ -50,5 +51,29 @@ print(m.ceil(2.0083 * 100)/ 100, "\n")
 # using the floor() function with decimal places
 print(m.floor(2.0083))
 print(m.floor(2.0083 * 10) / 10)
-print(m.floor(2.0083 * 1000)/ 1000)
+print(m.floor(2.0083 * 1000)/ 1000, "\n")
 
+# formatting numbers
+fp_number = 1234.5678
+print("{:.2f}".format(fp_number))
+print("{:.4f}".format(fp_number))
+print("{:,.2f}".format(fp_number))
+print("{:10,.2f}".format(fp_number), "\n")
+
+int_number = 12345
+print("{:d}".format(int_number))
+print("{:,d}".format(int_number), "\n")
+
+fp_number = .12345
+print("{:.0%}".format(fp_number))
+print("{:.1%}".format(fp_number), "\n")
+
+fp_number = 12345.6789
+print("{:.2e}".format(fp_number))
+print("{:.4e}".format(fp_number), "\n")
+
+# set the locale to English/United States
+lc.setlocale(lc.LC_ALL, "us")
+print(lc.currency(12345.25, grouping=True))
+print(lc.format("%d", 12345, grouping=True))
+print(lc.format("%.2f", 12345.45, grouping=True))
