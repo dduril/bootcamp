c66f55a70440396100e44d7451752de48f75f8d5
Doug Duril <doug.duril@gmail.com>
2017-11-07 19:04:47 -0800

    Finish MySQL ch-19 examples

--- murach-mysql/ch-19.sql
+++ murach-mysql/ch-19.sql
@@ -15,6 +15,7 @@
    cd /usr/local/mysql/bin
 */
 
+
 /* Running the mysqldump program */
 /*
 -- for a single database
@@ -26,3 +27,72 @@ mysqldump --databases books movies sales mysql > /development/mysql/multiple-db-
 -- for all databases
 mysqldump --all-databases > /development/mysql/all-db-backup-11-07-2017.sql -u root -p
 */
+
+
+/* Using the mysql program to restore databases */
+/*
+-- for a single database
+mysql movies < /development/mysql/movies-db-backup-11-07-2017.sql -u root -p
+
+-- for multiple databases
+mysql < /development/mysql/movies-db-backup-11-07-2017.sql -u root -p
+*/
+
+
+/* Using SELECT to export data to a file */
+USE media;
+
+-- tab-delimited file
+SELECT *
+INTO OUTFILE '/development/mysql/movie_tab.txt'
+FROM movie;
+
+-- comma-delimited file
+SELECT *
+INTO OUTFILE '/development/mysql/movie_comma.txt'
+FIELDS TERMINATED BY ','
+       ENCLOSED BY '"'
+       ESCAPED BY '\\'    
+FROM movie;
+
+
+/* Using LOAD DATA to import data from a file */
+USE ap;
+TRUNCATE vendor_contacts;
+
+LOAD DATA INFILE '/development/mysql/vendor_contacts_tab.txt'
+INTO TABLE vendor_contacts;
+
+SELECT * FROM vendor_contacts;
+TRUNCATE vendor_contacts;
+
+LOAD DATA INFILE '/development/mysql/vendor_contacts_comma.txt'
+INTO TABLE vendor_contacts
+FIELDS TERMINATED BY ','
+       ENCLOSED BY '"'
+       ESCAPED BY '\\';
+
+SELECT * FROM vendor_contacts;
+
+
+/* Using the CHECK TABLE statement */
+USE ap;
+
+-- checking a single table
+CHECK TABLE vendors;
+ 
+-- checking multiple tables and views
+CHECK TABLE vendors, invoices, terms, invoices_outstanding;
+ 
+-- using an option
+CHECK TABLE vendors, invoices FAST;
+
+/* Using the REPAIR TABLE statement */
+
+USE ap;
+
+-- repairing a single table
+REPAIR TABLE vendors;
+
+-- repairing two tables and using an option
+REPAIR TABLE vendors, invoices QUICK;
