94584107a66dfe9d0e2355c325ad754866f6202b
Doug Duril <doug.duril@gmail.com>
2017-11-14 10:53:17 -0800

    Add Python ch-10 examples

--- murach-python/ch-10.py
+++ murach-python/ch-10.py
@@ -2,3 +2,23 @@
 # Chapter 10 Code Examples
 #   Working with Strings
 # -------------------------------------
+
+# using an index to access a character of a string
+str1 = "Hello World!"
+print(str1)
+print("str1[0]  : ", str1[0])
+print("str1[1]  : ", str1[1])
+print("str1[6]  : ", str1[6])
+print("str1[11] : ", str1[11], "\n")
+for c in str1:
+    print(c)
+print("\n")
+
+# slicing a string
+str2 = "Hello World, Again"
+print(str2)
+print("str2[:5]   : ", str2[:5])
+print("str2[6:11] : ", str2[6:11])
+print("str2[13:]  : ", str2[13:])
+print("str2[:]    : ", str2[:])
+
