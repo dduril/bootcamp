67149ae7110b60a4c17dcd95ae100a5ebd9f3621
Doug Duril <doug.duril@gmail.com>
2017-11-15 13:05:58 -0800

    Finish Python ch-11 examples

--- murach-python/ch-11.py
+++ murach-python/ch-11.py
@@ -21,3 +21,22 @@ dt = datetime.strptime("11/15/2017 9:30:45", "%m/%d/%Y %H:%M:%S")
 print(dt)
 
 # using strftime (format)
+holiday_brunch = datetime(2017, 11, 23, 8, 30)
+holiday_lunch = datetime(2017, 11, 23, 14, 0)
+
+print("Thanksgiving Brunch: " + holiday_brunch.strftime("%Y-%m-%d (%A) at %I:%M %p"))
+print("Thanksgiving Lunch: " + holiday_lunch.strftime("%Y-%m-%d (%A) at %I:%M %p") + "\n")
+print("Thanksgiving Lunch: " + holiday_lunch.strftime("%c"))
+print("Thanksgiving Lunch: " + holiday_lunch.strftime("%x (%B, %A)"))
+print("\n")
+
+# returning parts of a date/time object
+thanksgiving = datetime(2017, 11, 23, 15, 30, 0)
+year = thanksgiving.year
+month = thanksgiving.month
+day = thanksgiving.day
+hour = thanksgiving.hour
+minute = thanksgiving.minute
+second = thanksgiving.second
+microsecond = thanksgiving.microsecond
+print(year, month, day, hour, minute, second, microsecond)
